f4879943d8411a16844b7fba4fc691aabaa34901
lciucu <luiza.adelina.ciucu@cern.ch>
2019-08-31 13:58:11 +0200

    define several NN arhitectures and loop over them to train several NN and store their results

--- MLUnfolding.py
+++ MLUnfolding.py
@@ -38,21 +38,43 @@ maxValue=500.0 # in GeV,
 binWidth=10.0 # in GeV
 
 # output
-outputFolderName="./output4"
+outputFolderName="./output5"
 
 # for DNN training with Keras
 doTrainNN=True
-batch_size=1000
-epochs=5
-kappa=8
+# the order is batch_size, epochs, kappa
+list_list_option=[
+    [1000,50,1],
+    [1000,50,2],
+    [1000,50,4],
+    [1000,50,8],
+    [1000,100,1],
+    [1000,100,2],
+    [1000,100,4],
+    [1000,100,8],
+]
 
-modelFileNameSuffix="batchSize_"+str(batch_size)+"_epochs_"+str(epochs)+"_"+str(kappa)+".hdf5"
-modelFileName=outputFolderName+"/model_full_"+modelFileNameSuffix
-weightsFileName=outputFolderName+"/model_weights_"+modelFileNameSuffix
+# for test
+if True:
+    list_list_option=[
+        [1000,300,8],
+    ]
+# done if
 
 #########################################################################################################
 #### Functions
 #######################################################################################################
+
+def get_NNFileNames(outputFolderName,batch_size,epochs,kappa):
+    modelFileNameSuffix="batchSize_"+str(batch_size)+"_epochs_"+str(epochs)+"_"+str(kappa)+".hdf5"
+    modelFileName=outputFolderName+"/model_full_"+modelFileNameSuffix
+    weightsFileName=outputFolderName+"/model_weights_"+modelFileNameSuffix
+    if debug:
+        print("modelFileName",modelFileName)
+        print("weightsFileName",weightsFileName)
+    # done if
+    return modelFileName,weightsFileName
+# done function
 
 # a general function to print the values and other properties of a numpy array
 # use to see the values of the numpy arrays in our code for debugging and understanding the code flow
@@ -190,7 +212,7 @@ def prepareModel(NBins,nvar=1,kappa=8):
     # here do we flatten the first layer?
     model.add(Flatten())
     # add the second layer with a relu activation function
-    model.add(Dense(kappa*NBins**2,activation='relu'))
+    model.add(Dense(kappa*NBins,activation='relu'))
     # add the thir layer, or the output, with a softmax activation function
     model.add(Dense(NBins,activation='softmax'))
     # compile the model by choosing the optimiser and the metrics
@@ -238,65 +260,74 @@ def doItAll():
     print_nparray("particleLevel","test","gtcat",gtcat)
     print_nparray("nominal","train","rf",rf)
     print_nparray("nominal","test","rt",rt)
-    # prepare the model of the DNN with Keras
-    model=prepareModel(NBins,nvar=1,kappa=kappa)
-    # let's train the NN only once and then save the weights to a file
-    # if already trained, then we can simply read the weights and continue working from there
-    # that way it is faster
-    if doTrainNN:
-        # fit the model
-        h = model.fit(rf,gfcat,batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(rt,gtcat))
-        # save model to not spend the time every time to retrain, as it takes some time
-        # a simple save
-        model.save(modelFileName)
-        # save also only the weights
-        model.save_weights(weightsFileName)
-    # done if
-    out=model.predict(rt)
-    # read as model2 with the same structure as before, but read the weights
-    model2=prepareModel(NBins,nvar=1,kappa=kappa)
-    model2.load_weights(weightsFileName)
-    out2=model2.predict(rt)
     #
-    if verbose:
-        print_nparray("nominal","test","rt",rt)
-        print_nparray("particleLevel","test","gt",gt)
-        # by printing we see that these two should be the same, they have shape (21538,50)
-        # while gtcat has terms strictly as zero and 1, out has terms that are very small like 10^-4
-        # so let's loop over all the elements and for each get the list with 50 elements, and then loop one by one and compare them
-        # but accepting that 10^-4 is close enough to zero and seeing if they get the element close to 1 at the same possition or close
-        print_nparray("particleLevel","test","gtcat",gtcat)
-        print_nparray("particleLevel","test","out",out)
-        assert(gtcat.shape[0]==out.shape[0])
-        assert(gtcat.shape[1]==out.shape[1])
-        for i in range(gtcat.shape[0]):
-            if i>5:
-                continue
-            gtcat_current=gtcat[i]
-            out_current=out[i]
-            out2_current=out2[i]
-            print_nparray("particleLevel","test","gtcat[i]",gtcat[i])
-            print_nparray("particleLevel","test","out[i]",out[i])
-            print_nparray("particleLevel","test","out2[i]",out2[i])
-            for j in range(gtcat_current.shape[0]):
-                # if True:
-                if gtcat_current[j]>0.1:
-                    print(i,j,"gtcat[i][j]",gtcat_current[j])
-            # done for loop over j
-            for j in range(out_current.shape[0]):
-                # if True:
-                if out_current[j]>0.1:
-                    print(i,j,"out[i][j]",out_current[j])
-                # done if
-            # done for loop over j
-            for j in range(out2_current.shape[0]):
-                # if True:
-                if out2_current[j]>0.1:
-                    print(i,j,"out2[i][j]",out2_current[j])
-                # done if
-            # done for loop over j
-        # done for loop over i
-        
+    # do the NN for different options, so we put all in a for loop
+    for list_option in list_list_option:
+        batch_size=list_option[0]
+        epochs=list_option[1]
+        kappa=list_option[2]
+        if verbose:
+            print("Start do NN part for batch_size",str(batch_size),"epochs",str(epochs),"kappa",str(kappa))
+        modelFileName,weightsFileName=get_NNFileNames(outputFolderName,batch_size,epochs,kappa)
+        # prepare the model of the DNN with Keras
+        model=prepareModel(NBins,nvar=1,kappa=kappa)
+        # let's train the NN only once and then save the weights to a file
+        # if already trained, then we can simply read the weights and continue working from there
+        # that way it is faster
+        if doTrainNN:
+            # fit the model
+            h = model.fit(rf,gfcat,batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(rt,gtcat))
+            # save model to not spend the time every time to retrain, as it takes some time
+            # a simple save
+            model.save(modelFileName)
+            # save also only the weights
+            model.save_weights(weightsFileName)
+        # done if
+        out=model.predict(rt)
+        # read as model2 with the same structure as before, but read the weights
+        model2=prepareModel(NBins,nvar=1,kappa=8)
+        model2.load_weights(weightsFileName)
+        out2=model2.predict(rt)
+        #
+        if verbose:
+            print_nparray("nominal","test","rt",rt)
+            print_nparray("particleLevel","test","gt",gt)
+            # by printing we see that these two should be the same, they have shape (21538,50)
+            # while gtcat has terms strictly as zero and 1, out has terms that are very small like 10^-4
+            # so let's loop over all the elements and for each get the list with 50 elements, and then loop one by one and compare them
+            # but accepting that 10^-4 is close enough to zero and seeing if they get the element close to 1 at the same possition or close
+            print_nparray("particleLevel","test","gtcat",gtcat)
+            print_nparray("particleLevel","test","out",out)
+            assert(gtcat.shape[0]==out.shape[0])
+            assert(gtcat.shape[1]==out.shape[1])
+            for i in range(gtcat.shape[0]):
+                if i>5:
+                    continue
+                gtcat_current=gtcat[i]
+                out_current=out[i]
+                out2_current=out2[i]
+                print_nparray("particleLevel","test","gtcat[i]",gtcat[i])
+                print_nparray("particleLevel","test","out[i]",out[i])
+                print_nparray("particleLevel","test","out2[i]",out2[i])
+                for j in range(gtcat_current.shape[0]):
+                    # if True:
+                    if gtcat_current[j]>0.1:
+                        print(i,j,"gtcat[i][j]",gtcat_current[j])
+                # done for loop over j
+                for j in range(out_current.shape[0]):
+                    # if True:
+                    if out_current[j]>0.1:
+                        print(i,j,"out[i][j]",out_current[j])
+                    # done if
+                # done for loop over j
+                for j in range(out2_current.shape[0]):
+                    # if True:
+                    if out2_current[j]>0.1:
+                        print(i,j,"out2[i][j]",out2_current[j])
+                    # done if
+                # done for loop over j
+            # done for loop over i
+        # done loop over list_option
     # all done
 # done function
 
